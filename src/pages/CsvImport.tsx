0644bd4b63be3ae99acafe34178601550d2b180a
ucchie <dbowserj@gmail.com>
2023-02-09 22:53:43 +0900

    📦 MaterialReactTable追加

--- src/pages/CsvImport.tsx
+++ src/pages/CsvImport.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "../App.css";
 import { TextField } from "@mui/material";
+import { useQuery } from "@apollo/client";
+import { NotesDocument } from "../graphql/generated/graphql";
 
 const CsvImport = () => {
+  const { data } = useQuery(NotesDocument);
+  data?.musics.map((data) => console.log(data.title));
   return (
     <>
       <div className="CsvImport">あいうえお</div>
