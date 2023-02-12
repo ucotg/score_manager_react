397514866bc6e661bdcdfd2dc507134a4066268c
ucchie <dbowserj@gmail.com>
2023-02-13 02:43:20 +0900

    📝 DJランク差分が正しく表示されるようにする

--- src/functions/djRank.ts
+++ src/functions/djRank.ts
@@ -2,40 +2,59 @@ export const djRank = (showBody: boolean, isBody: boolean, notes: number, exscor
   const max = notes * 2;
   const percentage: number = exscore / max;
 
-  if (percentage < 2 / 9) {
-    if (showBody) return !isBody ? "F+" : `${Math.ceil(exscore - (max * 1) / 9)}`;
-    return !isBody ? "E-" : `${Math.ceil((max * 2) / 9) - exscore}`;
+  if (exscore === 0) {
+    return "NO PLAY";
   }
-  if (percentage >= 2 / 9 && percentage < 1 / 3) {
-    if (showBody) return !isBody ? "E+" : `${Math.ceil(exscore - (max * 2) / 9)}`;
-    return !isBody ? "D-" : `${Math.ceil((max * 1) / 3) - exscore}`;
+  if (percentage > 0 && percentage < 1 / 9) {
+    return `F+${exscore}`;
   }
-  if (percentage >= 1 / 3 && percentage < 4 / 9) {
-    if (showBody) return !isBody ? "D+" : `${Math.ceil(exscore - (max * 1) / 3)}`;
-    return !isBody ? "C-" : `${Math.ceil((max * 4) / 9) - exscore}`;
+  if (percentage >= 1 / 9 && percentage < 2 / 9) {
+    return `E-${Math.ceil((max * 2) / 9) - exscore}`;
   }
-  if (percentage >= 4 / 9 && percentage < 5 / 9) {
-    if (showBody) return !isBody ? "C+" : `${Math.ceil(exscore - (max * 4) / 9)}`;
-    return !isBody ? "B-" : `${Math.ceil((max * 5) / 9) - exscore}`;
+  if (percentage >= 2 / 9 && percentage < 2.5 / 9) {
+    return `E+${exscore - Math.ceil((max * 2) / 9)}`;
   }
-  if (percentage >= 5 / 9 && percentage < 2 / 3) {
-    if (showBody) return !isBody ? "B+" : `${Math.ceil(exscore - (max * 5) / 9)}`;
-    return !isBody ? "A-" : `${Math.ceil((max * 2) / 3) - exscore}`;
+  if (percentage >= 2.5 / 9 && percentage < 3 / 9) {
+    return `D-${Math.ceil((max * 3) / 9) - exscore}`;
   }
-  if (percentage >= 2 / 3 && percentage < 7 / 9) {
-    if (showBody) return !isBody ? "A+" : `${Math.ceil(exscore - (max * 2) / 3)}`;
-    return !isBody ? "AA-" : `${Math.ceil((max * 7) / 9) - exscore}`;
+  if (percentage >= 3 / 9 && percentage < 3.5 / 9) {
+    return `D+${exscore - Math.ceil((max * 3) / 9)}`;
   }
-  if (percentage >= 7 / 9 && percentage < 8 / 9) {
-    if (showBody) return !isBody ? "AA+" : `${Math.ceil(exscore - (max * 7) / 9)}`;
-    return !isBody ? "AAA-" : `${Math.ceil((max * 8) / 9) - exscore}`;
+  if (percentage >= 3.5 / 9 && percentage < 4 / 9) {
+    return `C-${Math.ceil((max * 4) / 9) - exscore}`;
   }
-  if (percentage >= 8 / 9 && percentage < 17 / 18) {
-    if (showBody) return !isBody ? "MAX-" : `${max - exscore}`;
-    return !isBody ? "AAA+" : `${exscore - Math.ceil((max * 8) / 9)}`;
+  if (percentage >= 4 / 9 && percentage < 4.5 / 9) {
+    return `C+${exscore - Math.ceil((max * 4) / 9)}`;
+  }
+  if (percentage >= 4.5 / 9 && percentage < 5 / 9) {
+    return `B-${Math.ceil((max * 5) / 9) - exscore}`;
+  }
+  if (percentage >= 5 / 9 && percentage < 5.5 / 9) {
+    return `B+${exscore - Math.ceil((max * 5) / 9)}`;
+  }
+  if (percentage >= 5.5 / 9 && percentage < 6 / 9) {
+    return `A-${Math.ceil((max * 6) / 9) - exscore}`;
+  }
+  if (percentage >= 6 / 9 && percentage < 6.5 / 9) {
+    return `A+${exscore - Math.ceil((max * 6) / 9)}`;
+  }
+  if (percentage >= 6.5 / 9 && percentage < 7 / 9) {
+    return `AA-${Math.ceil((max * 7) / 9) - exscore}`;
+  }
+  if (percentage >= 7 / 9 && percentage < 7.5 / 9) {
+    return `AA+${exscore - Math.ceil((max * 7) / 9)}`;
+  }
+  if (percentage >= 7.5 / 9 && percentage < 8 / 9) {
+    return `AAA-${Math.ceil((max * 8) / 9) - exscore}`;
+  }
+  if (percentage >= 8 / 9 && percentage < 8.5 / 9) {
+    return `AAA+${exscore - Math.ceil((max * 8) / 9)}`;
   }
   if (percentage >= 17 / 18) {
     return `MAX-${max - exscore}`;
+  }
+  if (exscore === max) {
+    return `MAX+0`;
   }
   return "";
 };
