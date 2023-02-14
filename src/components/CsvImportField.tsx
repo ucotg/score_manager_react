3bea380d9d68d4cebdc46d410b3ca0eefaee6ba6
ucchie <dbowserj@gmail.com>
2023-02-15 04:32:40 +0900

    ⚡️ CSVインポートをuseFormを用いたものに変更

--- src/components/CsvImportField.tsx
+++ src/components/CsvImportField.tsx
@@ -1,34 +1,54 @@
 import React, { useContext, useState } from "react";
-import { Box, Typography, TextField, Button } from "@mui/material";
+import { Box, Typography, TextField, Button, CircularProgress } from "@mui/material";
+import { useForm, SubmitHandler, FieldErrors, DefaultValues } from "react-hook-form";
+
 import { csvSongData } from "../App";
+import { getSampleCsv } from "../functions/getSmapleCsv";
+
+type Input = {
+  csv: string;
+};
 
 export const CsvImportField: React.FC = () => {
   const [raw, setRaw] = useState<string>("");
   const { setCsv } = useContext(csvSongData);
+  const {
+    setValue,
+    register,
+    reset,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+    handleSubmit,
+  } = useForm<Input>();
+
+  const onSubmit: SubmitHandler<Input> = (data) => {
+    setCsv(data.csv);
+    reset();
+  };
 
   return (
-    <>
+    <form noValidate onSubmit={handleSubmit(onSubmit)}>
       <TextField
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setRaw(e.target.value);
-        }}
+        {...register("csv")}
         style={{ width: "100%" }}
-        value={raw}
         margin="dense"
         variant="outlined"
         multiline
         maxRows="8"
       />
+      <Button type="submit" variant="outlined" size="large" disabled={isSubmitting}>
+        {!isSubmitting ? "インポート" : <CircularProgress size={19} />}
+      </Button>
       <Button
         type="button"
         variant="outlined"
         size="large"
         onClick={() => {
-          setCsv(raw);
+          setRaw(getSampleCsv());
+          console.log(getSampleCsv());
         }}
       >
-        インポート
+        サンプルCSVを入力
       </Button>
-    </>
+    </form>
   );
 };
