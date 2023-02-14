b5b5d0ab4b6854f3cf476c11d2e7c18080588a77
ucchie <dbowserj@gmail.com>
2023-02-15 04:33:31 +0900

    ⚡️ 曲データをレベルでソートできるようにした

--- src/pages/MusicData.tsx
+++ src/pages/MusicData.tsx
@@ -43,7 +43,7 @@ const Register: React.FC = () => {
       header: "Lv",
       accessorKey: "level",
       size: 20,
-      enableSorting: false,
+      enableSorting: true,
       Filter: ({ header }) => (
         <TextField
           onChange={(e) => header.column.setFilterValue(e.target.value || undefined)}
@@ -95,14 +95,38 @@ const Register: React.FC = () => {
     { header: "DJ RANK", accessorKey: "djRank", size: 50 },
     { header: "SCORE RATE", accessorKey: "scoreRate", size: 50 },
     { header: "バージョン", accessorKey: "version", size: 50 },
+    {
+      header: "BPL GENRE",
+      accessorKey: "genre",
+      size: 50,
+      enableSorting: false,
+      Filter: ({ header }) => (
+        <TextField
+          onChange={(e) => header.column.setFilterValue(e.target.value || undefined)}
+          select
+          value={header.column.getFilterValue() ?? ""}
+          margin="none"
+          placeholder="Filter"
+          variant="standard"
+          fullWidth
+        >
+          {/*@ts-ignore*/}
+          <MenuItem value={null}>All</MenuItem>
+          <MenuItem value="NOTES">NOTES</MenuItem>
+          <MenuItem value="PEAK">PEAK</MenuItem>
+          <MenuItem value="SCRATCH">SCRATCH</MenuItem>
+          <MenuItem value="SOF-LAN">SOF-LAN</MenuItem>
+          <MenuItem value="CHARGE">CHARGE</MenuItem>
+          <MenuItem value="CHORD">CHORD</MenuItem>
+        </TextField>
+      ),
+      filterFn: (row, _columnIds, filterValue) => row.getValue<string>("genre").toString() === filterValue,
+    },
   ];
 
   return (
     <>
-      <Typography variant="h1">楽曲一覧</Typography>
-      <Typography variant="h3">
-        ログインは<Link to={`/login/`}>こちら</Link>
-      </Typography>
+      <Typography variant="h2">楽曲一覧</Typography>
       <Typography variant="h3">
         <Link to={`/`}>ホームに戻る</Link>
       </Typography>
