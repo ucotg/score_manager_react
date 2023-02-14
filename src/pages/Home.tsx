09cbfc9815c5f401911ea3d3cd595c021e90cfc0
ucchie <dbowserj@gmail.com>
2023-02-15 04:40:54 +0900

    📝 先にCSVをインポートするように警告

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
 
 const Home: React.FC = () => {
   return (
     <>
-      <h1>beatmaniaIIDX BPL課題曲スコア・オプションマネージャー</h1>
+      <Typography variant="h4">beatmaniaIIDX BPL課題曲スコア・オプションマネージャー</Typography>
       <div>
-        楽曲一覧は<Link to={`/musicData/`}>こちら</Link> <br />
+        楽曲一覧は<Link to={`/musicData/`}>こちら</Link>
+        <Typography color="red">※最初にCSVインポートをしないと楽曲一覧が表示されません！！※</Typography>
         CSVインポートは<Link to={`/csvImport/`}>こちら</Link>
       </div>
     </>
